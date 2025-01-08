cc47c310e3b2aaab2ba36a3fd0307eecad7da712
jayandhami007 <jayandhami007@gmail.com>
2025-01-08 10:38:14 +0545

    added new middle container for( in progress )section

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let lists = document.getElementsByClassName("list");
 let rightBox = document.getElementById("right");
+let middleBox= document.getElementById("middle")
 let leftBox = document.getElementById("left");
 for (lis of lists) {
   lis.addEventListener("dragstart", function (e) {
@@ -9,6 +10,13 @@ for (lis of lists) {
     });
     rightBox.addEventListener("drop", function (e) {
       rightBox.appendChild(selected);
+      selected = null;
+    });
+    middleBox.addEventListener("dragover", function (e) {
+      e.preventDefault();
+    });
+    middleBox.addEventListener("drop", function (e) {
+      middleBox.appendChild(selected);
       selected = null;
     });
     leftBox.addEventListener("dragover", function (e) {
